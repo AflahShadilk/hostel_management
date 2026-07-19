90f035b948821a297fe7e2d8351ad6d7537817e3
Aflah Shadil k <shadilpml@gmail.com>
2026-07-19 11:19:05 +0530

    feat(rent): implement rent repository layer

---
+++ lib/features/rent/data/repositories/rent_repository_impl.dart
@@ -0,0 +1,205 @@
+import '../../domain/entities/checkout_settlement_entity.dart';
+import '../../domain/entities/damage_charge_entity.dart';
+import '../../domain/entities/deposit_entity.dart';
+import '../../domain/entities/payment_entity.dart';
+import '../../domain/entities/receipt_entity.dart';
+import '../../domain/entities/rent_record_entity.dart';
+import '../../domain/entities/stay_entity.dart';
+import '../../domain/repositories/rent_repository.dart';
+import '../datasources/rent_local_datasource.dart';
+import '../models/checkout_settlement_model.dart';
+import '../models/damage_charge_model.dart';
+import '../models/deposit_model.dart';
+import '../models/payment_model.dart';
+import '../models/receipt_model.dart';
+import '../models/rent_record_model.dart';
+import '../models/stay_model.dart';
+
+class RentRepositoryImpl implements RentRepository {
+  final RentLocalDataSource _localDataSource;
+
+  const RentRepositoryImpl(this._localDataSource);
+
+  @override
+  Future<StayEntity> createStay(StayEntity stay) async =>
+      (await _localDataSource.createStay(StayModel.fromEntity(stay))).toEntity();
+
+  @override
+  Future<StayEntity?> getStayById(int id) async =>
+      (await _localDataSource.getStayById(id))?.toEntity();
+
+  @override
+  Future<StayEntity?> getActiveStayByTenantId(int tenantId) async =>
+      (await _localDataSource.getActiveStayByTenantId(tenantId))?.toEntity();
+
+  @override
+  Future<List<StayEntity>> getAllStays() async =>
+      (await _localDataSource.getAllStays()).map((model) => model.toEntity()).toList();
+
+  @override
+  Future<StayEntity> updateStay(StayEntity stay) async =>
+      (await _localDataSource.updateStay(StayModel.fromEntity(stay))).toEntity();
+
+  @override
+  Future<void> deleteStay(int id) => _localDataSource.deleteStay(id);
+
+  @override
+  Future<RentRecordEntity> createRentRecord(RentRecordEntity rentRecord) async =>
+      (await _localDataSource.createRentRecord(RentRecordModel.fromEntity(rentRecord)))
+          .toEntity();
+
+  @override
+  Future<RentRecordEntity?> getRentRecordById(int id) async =>
+      (await _localDataSource.getRentRecordById(id))?.toEntity();
+
+  @override
+  Future<List<RentRecordEntity>> getRentRecordsByStayId(int stayId) async =>
+      (await _localDataSource.getRentRecordsByStayId(stayId))
+          .map((model) => model.toEntity())
+          .toList();
+
+  @override
+  Future<List<RentRecordEntity>> getAllRentRecords() async =>
+      (await _localDataSource.getAllRentRecords())
+          .map((model) => model.toEntity())
+          .toList();
+
+  @override
+  Future<RentRecordEntity> updateRentRecord(RentRecordEntity rentRecord) async =>
+      (await _localDataSource.updateRentRecord(RentRecordModel.fromEntity(rentRecord)))
+          .toEntity();
+
+  @override
+  Future<void> deleteRentRecord(int id) => _localDataSource.deleteRentRecord(id);
+
+  @override
+  Future<PaymentEntity> createPayment(PaymentEntity payment) async =>
+      (await _localDataSource.createPayment(PaymentModel.fromEntity(payment))).toEntity();
+
+  @override
+  Future<PaymentEntity?> getPaymentById(int id) async =>
+      (await _localDataSource.getPaymentById(id))?.toEntity();
+
+  @override
+  Future<List<PaymentEntity>> getPaymentsByRentRecordId(int rentRecordId) async =>
+      (await _localDataSource.getPaymentsByRentRecordId(rentRecordId))
+          .map((model) => model.toEntity())
+          .toList();
+
+  @override
+  Future<List<PaymentEntity>> getAllPayments() async =>
+      (await _localDataSource.getAllPayments()).map((model) => model.toEntity()).toList();
+
+  @override
+  Future<PaymentEntity> updatePayment(PaymentEntity payment) async =>
+      (await _localDataSource.updatePayment(PaymentModel.fromEntity(payment))).toEntity();
+
+  @override
+  Future<void> deletePayment(int id) => _localDataSource.deletePayment(id);
+
+  @override
+  Future<ReceiptEntity> createReceipt(ReceiptEntity receipt) async =>
+      (await _localDataSource.createReceipt(ReceiptModel.fromEntity(receipt))).toEntity();
+
+  @override
+  Future<ReceiptEntity?> getReceiptByPaymentId(int paymentId) async =>
+      (await _localDataSource.getReceiptByPaymentId(paymentId))?.toEntity();
+
+  @override
+  Future<List<ReceiptEntity>> getAllReceipts() async =>
+      (await _localDataSource.getAllReceipts()).map((model) => model.toEntity()).toList();
+
+  @override
+  Future<ReceiptEntity> updateReceipt(ReceiptEntity receipt) async =>
+      (await _localDataSource.updateReceipt(ReceiptModel.fromEntity(receipt))).toEntity();
+
+  @override
+  Future<void> deleteReceipt(int id) => _localDataSource.deleteReceipt(id);
+
+  @override
+  Future<DepositEntity> createDeposit(DepositEntity deposit) async =>
+      (await _localDataSource.createDeposit(DepositModel.fromEntity(deposit))).toEntity();
+
+  @override
+  Future<DepositEntity?> getDepositByStayId(int stayId) async =>
+      (await _localDataSource.getDepositByStayId(stayId))?.toEntity();
+
+  @override
+  Future<List<DepositEntity>> getAllDeposits() async =>
+      (await _localDataSource.getAllDeposits()).map((model) => model.toEntity()).toList();
+
+  @override
+  Future<DepositEntity> updateDeposit(DepositEntity deposit) async =>
+      (await _localDataSource.updateDeposit(DepositModel.fromEntity(deposit))).toEntity();
+
+  @override
+  Future<void> deleteDeposit(int id) => _localDataSource.deleteDeposit(id);
+
+  @override
+  Future<DamageChargeEntity> createDamageCharge(
+    DamageChargeEntity damageCharge,
+  ) async =>
+      (await _localDataSource.createDamageCharge(
+        DamageChargeModel.fromEntity(damageCharge),
+      ))
+          .toEntity();
+
+  @override
+  Future<List<DamageChargeEntity>> getDamageChargesByStayId(int stayId) async =>
+      (await _localDataSource.getDamageChargesByStayId(stayId))
+          .map((model) => model.toEntity())
+          .toList();
+
+  @override
+  Future<List<DamageChargeEntity>> getAllDamageCharges() async =>
+      (await _localDataSource.getAllDamageCharges())
+          .map((model) => model.toEntity())
+          .toList();
+
+  @override
+  Future<DamageChargeEntity> updateDamageCharge(
+    DamageChargeEntity damageCharge,
+  ) async =>
+      (await _localDataSource.updateDamageCharge(
+        DamageChargeModel.fromEntity(damageCharge),
+      ))
+          .toEntity();
+
+  @override
+  Future<void> deleteDamageCharge(int id) =>
+      _localDataSource.deleteDamageCharge(id);
+
+  @override
+  Future<CheckoutSettlementEntity> createCheckoutSettlement(
+    CheckoutSettlementEntity checkoutSettlement,
+  ) async =>
+      (await _localDataSource.createCheckoutSettlement(
+        CheckoutSettlementModel.fromEntity(checkoutSettlement),
+      ))
+          .toEntity();
+
+  @override
+  Future<CheckoutSettlementEntity?> getCheckoutSettlementByStayId(
+    int stayId,
+  ) async =>
+      (await _localDataSource.getCheckoutSettlementByStayId(stayId))?.toEntity();
+
+  @override
+  Future<List<CheckoutSettlementEntity>> getAllCheckoutSettlements() async =>
+      (await _localDataSource.getAllCheckoutSettlements())
+          .map((model) => model.toEntity())
+          .toList();
+
+  @override
+  Future<CheckoutSettlementEntity> updateCheckoutSettlement(
+    CheckoutSettlementEntity checkoutSettlement,
+  ) async =>
+      (await _localDataSource.updateCheckoutSettlement(
+        CheckoutSettlementModel.fromEntity(checkoutSettlement),
+      ))
+          .toEntity();
+
+  @override
+  Future<void> deleteCheckoutSettlement(int id) =>
+      _localDataSource.deleteCheckoutSettlement(id);
+}
---
+++ lib/features/rent/domain/repositories/rent_repository.dart
@@ -0,0 +1,58 @@
+import '../entities/checkout_settlement_entity.dart';
+import '../entities/damage_charge_entity.dart';
+import '../entities/deposit_entity.dart';
+import '../entities/payment_entity.dart';
+import '../entities/receipt_entity.dart';
+import '../entities/rent_record_entity.dart';
+import '../entities/stay_entity.dart';
+
+abstract interface class RentRepository {
+  Future<StayEntity> createStay(StayEntity stay);
+  Future<StayEntity?> getStayById(int id);
+  Future<StayEntity?> getActiveStayByTenantId(int tenantId);
+  Future<List<StayEntity>> getAllStays();
+  Future<StayEntity> updateStay(StayEntity stay);
+  Future<void> deleteStay(int id);
+
+  Future<RentRecordEntity> createRentRecord(RentRecordEntity rentRecord);
+  Future<RentRecordEntity?> getRentRecordById(int id);
+  Future<List<RentRecordEntity>> getRentRecordsByStayId(int stayId);
+  Future<List<RentRecordEntity>> getAllRentRecords();
+  Future<RentRecordEntity> updateRentRecord(RentRecordEntity rentRecord);
+  Future<void> deleteRentRecord(int id);
+
+  Future<PaymentEntity> createPayment(PaymentEntity payment);
+  Future<PaymentEntity?> getPaymentById(int id);
+  Future<List<PaymentEntity>> getPaymentsByRentRecordId(int rentRecordId);
+  Future<List<PaymentEntity>> getAllPayments();
+  Future<PaymentEntity> updatePayment(PaymentEntity payment);
+  Future<void> deletePayment(int id);
+
+  Future<ReceiptEntity> createReceipt(ReceiptEntity receipt);
+  Future<ReceiptEntity?> getReceiptByPaymentId(int paymentId);
+  Future<List<ReceiptEntity>> getAllReceipts();
+  Future<ReceiptEntity> updateReceipt(ReceiptEntity receipt);
+  Future<void> deleteReceipt(int id);
+
+  Future<DepositEntity> createDeposit(DepositEntity deposit);
+  Future<DepositEntity?> getDepositByStayId(int stayId);
+  Future<List<DepositEntity>> getAllDeposits();
+  Future<DepositEntity> updateDeposit(DepositEntity deposit);
+  Future<void> deleteDeposit(int id);
+
+  Future<DamageChargeEntity> createDamageCharge(DamageChargeEntity damageCharge);
+  Future<List<DamageChargeEntity>> getDamageChargesByStayId(int stayId);
+  Future<List<DamageChargeEntity>> getAllDamageCharges();
+  Future<DamageChargeEntity> updateDamageCharge(DamageChargeEntity damageCharge);
+  Future<void> deleteDamageCharge(int id);
+
+  Future<CheckoutSettlementEntity> createCheckoutSettlement(
+    CheckoutSettlementEntity checkoutSettlement,
+  );
+  Future<CheckoutSettlementEntity?> getCheckoutSettlementByStayId(int stayId);
+  Future<List<CheckoutSettlementEntity>> getAllCheckoutSettlements();
+  Future<CheckoutSettlementEntity> updateCheckoutSettlement(
+    CheckoutSettlementEntity checkoutSettlement,
+  );
+  Future<void> deleteCheckoutSettlement(int id);
+}
